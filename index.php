1f693954a66d1d8ce50f23d146d10041db99700e
armin <adajic91@gmail.com>
2018-11-26 17:21:25 +0100

    Added modal message

--- index.php
+++ index.php
@@ -12,11 +12,15 @@
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
     <script type="text/javascript">
-		function setLayoutWidth() {
+		function loaded() {
 			doc_width = $(document).width()*90./100;
 			$('#layout_table').attr('width', doc_width);
 			nav_link_el = $("a[title='Nav Link 1']")[0];
 			$(nav_link_el).css({"text-decoration": "underline"});
+			$('.modal-toggle').on('click', function(e) {
+			  e.preventDefault();
+			  $('.modal').toggleClass('is-visible');
+			});
 		}		
 		
 		function subscribe() {
@@ -28,7 +32,9 @@
 			    contentType: "application/x-www-form-urlencoded",
 			    async: true,
 			    success: function(responseData, textStatus, jqXHR) {
-			        alert(responseData);
+			        $($(".modal-heading")[0]).html("Inscription Newsletter API Message");
+			        $($(".modal-text")[0]).html(responseData);
+			        $('.modal').toggleClass('is-visible');
 			    },
 			    error: function(jqXHR, textStatus, errorThrown) {
 			        console.log(jqXHR);
@@ -39,7 +45,7 @@
 		}
 	</script>
 </head>
-<body class="no_margin-no_padding" onload="setLayoutWidth();" style="font-family: Georgia, serif;">
+<body class="no_margin-no_padding" onload="loaded();" style="font-family: Georgia, serif;">
 	<center>
 	<noscript>
 	<br>Please enable Javascript to see the page
@@ -362,6 +368,23 @@
 			<hr style="height: 15px; width: 99%; margin:0 auto; background: none; border:0 none;"/>
 			Mentions légales –Nous contacter
 			<hr style="height: 50px; width: 99%; margin:0 auto; background: none; border:0 none;"/>
+			
+			<svg display="none" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="768" height="800" viewBox="0 0 768 800"><defs><g id="icon-close"><path class="path1" d="M31.708 25.708c-0-0-0-0-0-0l-9.708-9.708 9.708-9.708c0-0 0-0 0-0 0.105-0.105 0.18-0.227 0.229-0.357 0.133-0.356 0.057-0.771-0.229-1.057l-4.586-4.586c-0.286-0.286-0.702-0.361-1.057-0.229-0.13 0.048-0.252 0.124-0.357 0.228 0 0-0 0-0 0l-9.708 9.708-9.708-9.708c-0-0-0-0-0-0-0.105-0.104-0.227-0.18-0.357-0.228-0.356-0.133-0.771-0.057-1.057 0.229l-4.586 4.586c-0.286 0.286-0.361 0.702-0.229 1.057 0.049 0.13 0.124 0.252 0.229 0.357 0 0 0 0 0 0l9.708 9.708-9.708 9.708c-0 0-0 0-0 0-0.104 0.105-0.18 0.227-0.229 0.357-0.133 0.355-0.057 0.771 0.229 1.057l4.586 4.586c0.286 0.286 0.702 0.361 1.057 0.229 0.13-0.049 0.252-0.124 0.357-0.229 0-0 0-0 0-0l9.708-9.708 9.708 9.708c0 0 0 0 0 0 0.105 0.105 0.227 0.18 0.357 0.229 0.356 0.133 0.771 0.057 1.057-0.229l4.586-4.586c0.286-0.286 0.362-0.702 0.229-1.057-0.049-0.13-0.124-0.252-0.229-0.357z"></path></g></defs></svg>
+			<div class="modal">
+				<div class="modal-overlay modal-toggle"></div>
+				<div class="modal-wrapper modal-transition">
+					<div class="modal-header">
+						<button class="modal-close modal-toggle"><svg class="icon-close icon" viewBox="0 0 32 32"><use xlink:href="#icon-close"></use></svg></button>
+						<h2 class="modal-heading">--PLACE_HOLDER--</h2>
+					</div>
+					<div class="modal-body">
+						<div class="modal-content">
+							<p class="modal-text">--PLACE_HOLDER--</p>
+							<button class="modal-toggle">&nbsp;&nbsp;&nbsp;OK&nbsp;&nbsp;&nbsp;</button>
+						</div>
+					</div>
+				</div>
+			</div>
 		</td></tr>
 	</table>
 	</center>
